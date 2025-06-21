deb3ee394546f44deb47eff27ffd19d0c66322ec
Artem <artem.kotyakhov@gmail.com>
2025-06-21 20:15:49 +0300

    Add search book test

---
+++ tests/test_search_book.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from pages.main_page import MainPage
+
+
+def test_search_popular_book():
+    """Тест по поиску популярной книги включает:
+    авторизацию, переход в каталог с жанрами книг, выбор книги по установленным фильтрам, переход в корзину, переход к покупке книги."""
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('detach', True)
+    options.add_argument("--guest")
+    options.add_argument('--disable-notifications')
+    options.add_argument('--disable-popup-blocking')
+    driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+    print('Start Test: Search popular book')
+
+    mp = MainPage(driver)
+    mp.search_popular_book()
+
+    print('Finish Test: Search popular book')
+    driver.quit()
